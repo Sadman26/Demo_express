343faef2214ef79a721b95bbc6bf3f9cff64a08f
Sadman26 <fareless.ranger@gmail.com>
2023-11-06 22:47:03 +0600

    okay till now

--- test.js
+++ test.js
@@ -7,10 +7,10 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'temp/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/page2', (req, res) => {
-    res.sendFile(path.join(__dirname, 'temp/page2.html'))
+    res.sendFile(path.join(__dirname, 'page2.html'))
 });
 app.listen(3000, () => {
     console.log('Listening to port 3000...');
